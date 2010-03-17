82f845172dd2218d87939df592c78ddaf847a654
Ryan Stenhouse <ryan@ryanstenhouse.eu>
2010-03-17 20:00:53 +0000

    Making a good start on the tests.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,3 +7,36 @@ GPGR_ROOT = File.expand_path(File.dirname(__FILE__) + '/../lib')
 # Include the gpgr library
 #
 require GPGR_ROOT + '/gpgr.rb'
+
+
+
+# Helper methods for the tests.
+#
+
+def generate_key_for(email, name)
+  file_path = "/tmp/gpgr_testing_#{Time.now.to_i}"
+  params = <<-PGP
+Key-Type: DSA
+Key-Length: 1024
+Subkey-Type: ELG-E
+Subkey-Length: 1024
+Name-Real: #{name}
+Name-Comment: with stupid passphrase
+Name-Email: #{email}
+Expire-Date: 0
+Passphrase: abc
+%pubring #{file_path}.pub
+%secring #{file_path}.sec
+%commit
+  PGP
+  `echo '#{params}' | gpg --batch --gen-key -a`
+  if File.exists?(file_path + '.pub')
+    return file_path + '.pub'
+  else
+    raise "ARRGH"
+  end
+end
+
+def remove_installed_key(email)
+  system "gpg --delete-key --yes --batch #{email}"
+end
--- test/test_key_management_functionality.rb
+++ test/test_key_management_functionality.rb
@@ -2,8 +2,23 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 
 class TestKeyManagementFunctionality < Test::Unit::TestCase
 
-  def test_this_works
-    assert true
+  def setup
+    @john_key = generate_key_for('john@example.com','John Test')
+    @mark_key = generate_key_for('mark@example.com','Mark Test')
+  end
+  
+  def teardown
+    remove_installed_key 'john@example.com'
+    remove_installed_key 'mark@example.com'
+  end
+  
+  def test_import_keys_method_adds_keys
+    assert_equal false, Gpgr::Keys.installed_public_keys.include?('john@example.com')
+    assert_equal false, Gpgr::Keys.installed_public_keys.include?('mark@example.com')
+    Gpgr::Keys.import @john_key
+    Gpgr::Keys.import @mark_key
+    assert_equal true, Gpgr::Keys.installed_public_keys.include?('john@example.com')
+    assert_equal true, Gpgr::Keys.installed_public_keys.include?('mark@example.com')
   end
 
 end
